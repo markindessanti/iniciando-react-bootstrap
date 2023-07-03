0704db236adc4f4be59185d6336337877845ce30
Markin Dessanti <markindessanti@gmail.com>
2023-07-03 11:17:52 -0300

    Commit inicial

---
+++ src/App.js
@@ -0,0 +1,14 @@
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<h1>
+					Meu Aplicativo
+				</h1>
+				<p>Estudos de desevolvimento com React</p>
+			</header>
+		</div>
+	);
+}
+
+export default App;
