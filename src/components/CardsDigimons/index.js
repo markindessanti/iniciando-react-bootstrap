2603960c5560cacc1b73cae2b133b0c10f47a708
Markin Dessanti <markindessanti@gmail.com>
2023-07-04 16:12:23 -0300

    Limitação do número de cards Digimon exibidos

--- src/components/CardsDigimons/index.js
+++ src/components/CardsDigimons/index.js
@@ -25,20 +25,21 @@ class CardsDigimons extends Component {
 		return (
 			<>
 				{
-					this.state.digimons.map(digimon =>
-						<Col className='d-flex justify-content-center' sm={6} md={4} lg={3} xxl={2}>
-							<Card key={digimon.name} className='mb-3' style={{ width: '18rem' }}>
-								<Card.Img variant="top" src={digimon.img} />
-								<Card.Body>
-									<Card.Title>{digimon.name}</Card.Title>
-									<Card.Text className="d-flex">
-										<label className="fw-bold me-1" htmlFor="digimon-level">Level:</label>
-										<p name="digimon-level">{digimon.level}</p>
-									</Card.Text>
-									<Button variant="primary">Go somewhere</Button>
-								</Card.Body>
-							</Card>
-						</Col>
+					this.state.digimons.map((digimon, index) =>
+						index < 12 ?
+							<Col className='d-flex justify-content-center' sm={6} md={4} lg={3} xxl={2}>
+								<Card key={digimon.name} className='mb-3' style={{ width: '18rem' }}>
+									<Card.Img variant="top" src={digimon.img} />
+									<Card.Body>
+										<Card.Title>{digimon.name}</Card.Title>
+										<Card.Text className="d-flex">
+											<label className="fw-bold me-1" htmlFor="digimon-level">Level:</label>
+											<p name="digimon-level">{digimon.level}</p>
+										</Card.Text>
+										<Button variant="primary">Go somewhere</Button>
+									</Card.Body>
+								</Card>
+							</Col> : ""
 					)
 				}
 			</>
